488137719e1eb9d48243cd17ee78c0f9c7204233
James Strutt <10740291+Electrocyte@users.noreply.github.com>
2023-06-10 20:42:15 +0800

    code clean up

--- build_audiobook.py
+++ build_audiobook.py
@@ -46,152 +46,6 @@ def process_text(blocks) -> Tuple[List, int]:
 
     return LoD, len(text)
 
-# pipenv install EbookLib
-
-parser = argparse.ArgumentParser(description='Convert epub into script.')
-parser.add_argument('-a', '--apiKeyPath', help='Location of api key.')
-parser.add_argument('-f', '--folder', help='Location of saved files.')
-parser.add_argument('-s', '--savename', help='Save ID.')
-parser.add_argument('-e', '--Epub', help='Save ID.')
-args = parser.parse_args()
-apiKeyPath = args.apiKeyPath
-folder = args.folder
-savename = args.savename
-Epub = args.Epub
-
-target_book = f"{folder}/{Epub}"
-
-try:
-    ACCESS_TOKEN = GPT4_call.read_api_key(f"{apiKeyPath}/gpt4_api")
-except:
-    ACCESS_TOKEN = GPT4_call.read_api_key()
-
-try:
-    api_key = whisper.read_api_key(f"{apiKeyPath}/api_key")
-except:
-    api_key = whisper.read_api_key()
-
-openai.api_key = api_key
-
-# Load the epub file
-book = epub.read_epub(target_book)
-
-preprompt = "Given the following spoken passages, please assign a speaker name for the speaker marked as 'unknown'. \
-    ONLY return List1 with speakers, identified before the '$$$' in List1 in PYTHON format. \
-    Use context clues from the given speeches to determine the most likely speaker for the ones marked as 'unknown'. \
-        Replace 'unknown' with the correct speaker. \
-            The conversation is as follows, Alarak is the narrator, not a character:"
-
-postprompt = "Given the following spoken passages, please assign a speaker name for the speaker marked as 'unknown'. \
-    ONLY return List2 with speakers, identified after the '$$$' in List2 in PYTHON format. \
-    Use context clues from the given speeches to determine the most likely speaker for the ones marked as 'unknown'. \
-        Replace 'unknown' with the correct speaker. \
-            The conversation is as follows, Alarak is the narrator, not a character:"
-
-# Get a list of items of type ITEM_DOCUMENT
-document_items = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
-
-# Iterate over the items and print their content
-n = 0
-nn = 0
-for item in document_items:
-    # Decoding as 'utf-8'. You may need to adjust this if the text is in a different encoding.
-    content = item.get_content().decode('utf-8')
-
-    # Parse the content with BeautifulSoup
-    soup = BeautifulSoup(content, 'html.parser')
-
-    # Initialize an empty list to hold blocks of text
-    blocks = []
-    # Initialize a counter
-    count = 0
-    rolling_totals = 0
-
-    # Find elements with the desired classes
-    for p in soup.find_all('p', class_=['textfirst', 'calibre8', 'calibre7']):
-        # Append the text of the current element to the current block
-        blocks.append(p.get_text())
-        # Increment the counter
-        count += 1
-        rolling_total = len(p.get_text())
-        rolling_totals += rolling_total
-        # print(n, count, nn)
-
-        # If we've collected 10 elements, process them
-        if rolling_totals > 5000 and rolling_totals < 5500:
-            nn += 1
-            LoD, text = process_text(blocks)
-
-            half_total = rolling_totals / 2
-            new_dict = {"Speaker": "$$$", "Speech": "$$$"}
-            i_counter = 0
-            for e, i, in enumerate(LoD):
-                i_counter += len(i['Speech'])
-                if i_counter > half_total:
-                    LoD.insert(e, new_dict)
-                    break
-
-            p1 = LoD[0:e]
-            p2 = LoD[e:]
-
-            pp1 = f"List1, provide speakers for this: {p1}, List2, only use this for context: {p2}"
-            pp2 = f"List1, only use this for context: {p1}, List2, provide speakers for this: {p2}"
-
-            initial_file = f'{folder}/{savename}-epubfragment-{nn}.json'
-            with open(initial_file, 'w') as g:
-                json.dump(LoD, g)
-
-            pre_save_file = f'{folder}/{savename}-GPT-4-pre-{nn}.json'
-            post_save_file = f'{folder}/{savename}-GPT-4-post-{nn}.json'
-            # print(f"Saving to: {save_file} - Character count: {text}")
-
-            ## GPT-3.5
-            # if not os.path.exists(save_file):
-                # GPT_reply1 = call_GPT.direct_contact_GPT(LoD, prompt)
-                # # print("GPT_reply: ", GPT_reply)  # check what's inside GPT_reply
-
-                # the_reply1 = GPT_reply1['choices'][0]['message']['content']
-                # # print("the_reply: ", the_reply)  # check what's inside the_reply
-
-            ## GPT-4
-            if not os.path.exists(pre_save_file):
-                the_reply1 = GPT4_call.process_message(pp1, preprompt, ACCESS_TOKEN)
-
-                with open(pre_save_file, 'w') as f:
-                    json.dump([the_reply1], f)
-
-            ## GPT-4
-            if not os.path.exists(post_save_file):
-                the_reply2 = GPT4_call.process_message(pp2, postprompt, ACCESS_TOKEN)
-
-                with open(post_save_file, 'w') as f:
-                    json.dump([the_reply2], f)
-
-            # print(f"\n{the_reply1}\n")
-
-            # Reset blocks and counter for the next batch
-            blocks = []
-            count = 0
-            rolling_totals = 0
-            # break
-            if "3.json" in pre_save_file:
-                break
-
-
-        # need to account for an instance of over shoot !!!!!!!!!!!!!
-        # i.e. when rolling_totals > 8000
-        elif rolling_totals >= 5500 and rolling_totals < 8000:
-            LoD = process_text(blocks)
-            print(f"Too many characters, terminating.")
-            break
-
-        else:
-            pass
-
-    n += 1
-    if n > 8:
-        break
-
 
 def replacer(match):
     s = match.group()
@@ -276,6 +130,150 @@ def check_original(s, ori, _type_):
         return None
 
 
+# pipenv install EbookLib
+
+parser = argparse.ArgumentParser(description='Convert epub into script.')
+parser.add_argument('-a', '--apiKeyPath', help='Location of api key.')
+parser.add_argument('-f', '--folder', help='Location of saved files.')
+parser.add_argument('-s', '--savename', help='Save ID.')
+parser.add_argument('-e', '--Epub', help='Save ID.')
+args = parser.parse_args()
+apiKeyPath = args.apiKeyPath
+folder = args.folder
+savename = args.savename
+Epub = args.Epub
+
+target_book = f"{folder}/{Epub}"
+
+try:
+    ACCESS_TOKEN = GPT4_call.read_api_key(f"{apiKeyPath}/gpt4_api")
+except:
+    ACCESS_TOKEN = GPT4_call.read_api_key()
+
+try:
+    api_key = whisper.read_api_key(f"{apiKeyPath}/api_key")
+except:
+    api_key = whisper.read_api_key()
+
+openai.api_key = api_key
+
+# Load the epub file
+book = epub.read_epub(target_book)
+
+preprompt = "Given the following spoken passages, please assign a speaker name for the speaker marked as 'unknown'. \
+    ONLY return List1 with speakers, identified before the '$$$' in List1 in PYTHON format. \
+    Use context clues from the given speeches to determine the most likely speaker for the ones marked as 'unknown'. \
+        Replace 'unknown' with the correct speaker. \
+            The conversation is as follows, Alarak is the narrator, not a character:"
+
+postprompt = "Given the following spoken passages, please assign a speaker name for the speaker marked as 'unknown'. \
+    ONLY return List2 with speakers, identified after the '$$$' in List2 in PYTHON format. \
+    Use context clues from the given speeches to determine the most likely speaker for the ones marked as 'unknown'. \
+        Replace 'unknown' with the correct speaker. \
+            The conversation is as follows, Alarak is the narrator, not a character:"
+
+# Get a list of items of type ITEM_DOCUMENT
+document_items = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
+
+# Iterate over the items and print their content
+n = 0
+nn = 0
+for item in document_items:
+    # Decoding as 'utf-8'. You may need to adjust this if the text is in a different encoding.
+    content = item.get_content().decode('utf-8')
+
+    # Parse the content with BeautifulSoup
+    soup = BeautifulSoup(content, 'html.parser')
+
+    # Initialize an empty list to hold blocks of text
+    blocks = []
+    # Initialize a counter
+    count = 0
+    rolling_totals = 0
+
+    # Find elements with the desired classes
+    for p in soup.find_all('p', class_=['textfirst', 'calibre8', 'calibre7']):
+        # Append the text of the current element to the current block
+        blocks.append(p.get_text())
+        # Increment the counter
+        count += 1
+        rolling_total = len(p.get_text())
+        rolling_totals += rolling_total
+
+        # If we've collected 10 elements, process them
+        if rolling_totals > 5000 and rolling_totals < 5500:
+            nn += 1
+            LoD, text = process_text(blocks)
+
+            half_total = rolling_totals / 2
+            new_dict = {"Speaker": "$$$", "Speech": "$$$"}
+            i_counter = 0
+            for e, i, in enumerate(LoD):
+                i_counter += len(i['Speech'])
+                if i_counter > half_total:
+                    LoD.insert(e, new_dict)
+                    break
+
+            p1 = LoD[0:e]
+            p2 = LoD[e:]
+
+            pp1 = f"List1, provide speakers for this: {p1}, List2, only use this for context: {p2}"
+            pp2 = f"List1, only use this for context: {p1}, List2, provide speakers for this: {p2}"
+
+            initial_file = f'{folder}/{savename}-epubfragment-{nn}.json'
+            with open(initial_file, 'w') as g:
+                json.dump(LoD, g)
+
+            pre_save_file = f'{folder}/{savename}-GPT-4-pre-{nn}.json'
+            post_save_file = f'{folder}/{savename}-GPT-4-post-{nn}.json'
+            # print(f"Saving to: {save_file} - Character count: {text}")
+
+            ## GPT-3.5 - sadly you are just too stupid to do this important task :(
+            # if not os.path.exists(save_file):
+                # GPT_reply1 = call_GPT.direct_contact_GPT(LoD, prompt)
+                # # print("GPT_reply: ", GPT_reply)  # check what's inside GPT_reply
+
+                # the_reply1 = GPT_reply1['choices'][0]['message']['content']
+                # # print("the_reply: ", the_reply)  # check what's inside the_reply
+
+            ## GPT-4
+            if not os.path.exists(pre_save_file):
+                the_reply1 = GPT4_call.process_message(pp1, preprompt, ACCESS_TOKEN)
+
+                with open(pre_save_file, 'w') as f:
+                    json.dump([the_reply1], f)
+
+            ## GPT-4
+            if not os.path.exists(post_save_file):
+                the_reply2 = GPT4_call.process_message(pp2, postprompt, ACCESS_TOKEN)
+
+                with open(post_save_file, 'w') as f:
+                    json.dump([the_reply2], f)
+
+            # Reset blocks and counter for the next batch
+            blocks = []
+            count = 0
+            rolling_totals = 0
+            # break
+            # if "3.json" in pre_save_file:
+            #     break
+
+
+        # need to account for an instance of over shoot !!!!!!!!!!!!!
+        # i.e. when rolling_totals > 8000
+        elif rolling_totals >= 5500 and rolling_totals < 8000:
+            LoD = process_text(blocks)
+            print(f"Too many characters, terminating.")
+            break
+
+        else:
+            pass
+
+    n += 1
+    if n > 8:
+        break
+
+
 files = glob.glob(f'{folder}/{savename}-GPT-4-pre*.json')
 
 failed_files = []
@@ -294,10 +292,6 @@ for file in files:
 
             pre_s = clean_up(predata[i])
             post_s = clean_up(postdata[i])
-
-            # print(pre_s)
-            # print("\n\n")
-            # print(ori)
 
             # # parse the JSON string into Python list of dictionaries
             pre_part = check_original(pre_s, ori, "before")
