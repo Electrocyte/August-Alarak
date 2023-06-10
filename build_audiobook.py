6d5df6019c44f3050dade2ca802ff9fdb331763d
James Strutt <10740291+Electrocyte@users.noreply.github.com>
2023-06-10 20:39:45 +0800

    fragile WORKING version of audiobook script writer

--- build_audiobook.py
+++ build_audiobook.py
@@ -17,7 +17,7 @@ from typing import List, Dict, Any, Tuple
 def process_text(blocks) -> Tuple[List, int]:
     # Join the blocks of text into a single string
     text = ' '.join(blocks)
-    print(repr(text)) # see new lines
+    # print(repr(text)) # see new lines
 
     # Replace stylised quotes
     text = text.replace('‘', "'").replace('’', "'")
@@ -41,8 +41,8 @@ def process_text(blocks) -> Tuple[List, int]:
     # Use json.dumps to convert to a JSON formatted string
     json_string = json.dumps(LoD, indent=4)
 
-    # Print the result
-    print(json_string)
+    # # Print the result
+    # print(json_string)
 
     return LoD, len(text)
 
@@ -76,8 +76,17 @@ openai.api_key = api_key
 # Load the epub file
 book = epub.read_epub(target_book)
 
-# prompt = """Rewrite the following text into a list of dictionaries with the key 'Speaker' and value is always the speech, where 'Speaker' is the name of the speaker or narrator if no speaker is specified, and the value is any speech or narrated text. Replace stylized quotes in 'Speech' with regular quotes and mark the speaker as 'unknown' if not known. The text to be rewritten is: """
-prompt = "Given the following spoken passages, please assign a speaker name for the ones marked as 'unknown'. If Alarak is a 'Speaker', output an empty dictionary. Use context clues from the given speeches to determine the most likely speaker for the ones marked as 'unknown'. Replace 'unknown' with the correct speaker. The conversation is as follows, Alarak is the narrator, not a character:"
+preprompt = "Given the following spoken passages, please assign a speaker name for the speaker marked as 'unknown'. \
+    ONLY return List1 with speakers, identified before the '$$$' in List1 in PYTHON format. \
+    Use context clues from the given speeches to determine the most likely speaker for the ones marked as 'unknown'. \
+        Replace 'unknown' with the correct speaker. \
+            The conversation is as follows, Alarak is the narrator, not a character:"
+
+postprompt = "Given the following spoken passages, please assign a speaker name for the speaker marked as 'unknown'. \
+    ONLY return List2 with speakers, identified after the '$$$' in List2 in PYTHON format. \
+    Use context clues from the given speeches to determine the most likely speaker for the ones marked as 'unknown'. \
+        Replace 'unknown' with the correct speaker. \
+            The conversation is as follows, Alarak is the narrator, not a character:"
 
 # Get a list of items of type ITEM_DOCUMENT
 document_items = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
@@ -109,16 +118,32 @@ for item in document_items:
         # print(n, count, nn)
 
         # If we've collected 10 elements, process them
-        if rolling_totals > 3500 and rolling_totals < 4500:
+        if rolling_totals > 5000 and rolling_totals < 5500:
             nn += 1
             LoD, text = process_text(blocks)
+
+            half_total = rolling_totals / 2
+            new_dict = {"Speaker": "$$$", "Speech": "$$$"}
+            i_counter = 0
+            for e, i, in enumerate(LoD):
+                i_counter += len(i['Speech'])
+                if i_counter > half_total:
+                    LoD.insert(e, new_dict)
+                    break
+
+            p1 = LoD[0:e]
+            p2 = LoD[e:]
+
+            pp1 = f"List1, provide speakers for this: {p1}, List2, only use this for context: {p2}"
+            pp2 = f"List1, only use this for context: {p1}, List2, provide speakers for this: {p2}"
 
             initial_file = f'{folder}/{savename}-epubfragment-{nn}.json'
             with open(initial_file, 'w') as g:
                 json.dump(LoD, g)
 
-            save_file = f'{folder}/{savename}-GPT-4-{nn}.json'
-            print(f"Saving to: {save_file} - Character count: {text}")
+            pre_save_file = f'{folder}/{savename}-GPT-4-pre-{nn}.json'
+            post_save_file = f'{folder}/{savename}-GPT-4-post-{nn}.json'
+            # print(f"Saving to: {save_file} - Character count: {text}")
 
             ## GPT-3.5
             # if not os.path.exists(save_file):
@@ -129,12 +154,18 @@ for item in document_items:
                 # # print("the_reply: ", the_reply)  # check what's inside the_reply
 
             ## GPT-4
-            if not os.path.exists(save_file):
-                the_reply1 = GPT4_call.process_message(LoD, prompt, ACCESS_TOKEN)
-                print(the_reply1)
+            if not os.path.exists(pre_save_file):
+                the_reply1 = GPT4_call.process_message(pp1, preprompt, ACCESS_TOKEN)
 
-                with open(save_file, 'w') as f:
+                with open(pre_save_file, 'w') as f:
                     json.dump([the_reply1], f)
+
+            ## GPT-4
+            if not os.path.exists(post_save_file):
+                the_reply2 = GPT4_call.process_message(pp2, postprompt, ACCESS_TOKEN)
+
+                with open(post_save_file, 'w') as f:
+                    json.dump([the_reply2], f)
 
             # print(f"\n{the_reply1}\n")
 
@@ -142,12 +173,14 @@ for item in document_items:
             blocks = []
             count = 0
             rolling_totals = 0
-            break
+            # break
+            if "3.json" in pre_save_file:
+                break
 
 
         # need to account for an instance of over shoot !!!!!!!!!!!!!
         # i.e. when rolling_totals > 8000
-        elif rolling_totals >= 4500 and rolling_totals < 8000:
+        elif rolling_totals >= 5500 and rolling_totals < 8000:
             LoD = process_text(blocks)
             print(f"Too many characters, terminating.")
             break
@@ -165,51 +198,127 @@ def replacer(match):
     return s.replace("'", '"')
 
 
-# prompt_cleanup = "Please convert the following into a json item with a list of dictionaries, remove all newlines and additional '\\':"
+def clean_up(i):
+    result = re.sub(r"^.*:\s*\n", "", i, flags=re.MULTILINE)
+    result = re.sub(r'```python\nList1 = ', '', result)
+    result = re.sub(r"\n\w.*?$", "", result, flags=re.MULTILINE)
 
-files = glob.glob(f'{folder}/{savename}-GPT-4-*.json')
+    # remove newlines
+    s = re.sub(r'\n', '', result)
 
-parts = []
-for file in files:
-    number = file.split(".")[0].split("-")[-1]
-    ori_json = f'{folder}/{savename}-epubfragment-{number}.json'
-    with open(file, 'r', encoding='utf-8') as f, open(ori_json, 'r') as g:
-        ori = json.loads(g.read())
+    # replace single quotes with double quotes to make it valid JSON
+    s = re.sub(r"(\w)(\')(\w)", r"\1---\3", s)
+    s = re.sub(r"(\")(\')", r"\1---", s)
+    s = re.sub(r"(\')(\")", r"---\2", s)
+    s = re.sub(r"(\')(\.)", r"---\2", s)
 
-        data = json.load(f)
-        for i in data:
-            result = re.sub(r"^.*:\s*\n", "", i, flags=re.MULTILINE)
-            result = re.sub(r"\n\w.*?$", "", result, flags=re.MULTILINE)
-            # remove newlines
-            s = re.sub(r'\n', '', result)
+    # # replace single quotes with double quotes to make it valid JSON
+    s = re.sub(r"'.*?'", replacer, s)
+    s = s.replace('---', "'")
+    s = s.replace('"...,', '"},')
+    s = re.sub(r'\w$', '"}]', s)
+    s = re.sub(r'^\w.*\[', '[', s)
+    s = re.sub(r'\`\`\`python    ', '[', s)
+    s = re.sub(r'\`\`\`', '', s)
+    s = re.sub(r'\–$', '"}]', s)
 
-            # replace single quotes with double quotes to make it valid JSON
-            s = re.sub(r"(\w)(\')(\w)", r"\1---\3", s)
-            s = re.sub(r"(\")(\')", r"\1---", s)
-            s = re.sub(r"(\')(\")", r"---\2", s)
+    s = re.sub(r'^\s*\{"', '[{"', s)
 
-            # # replace single quotes with double quotes to make it valid JSON
-            s = re.sub(r"'.*?'", replacer, s)
+    return s
 
-            s = s.replace('---', "'")
 
-            # print("\n",s)
+def check_original(s, ori, _type_):
+    # find the index of dictionary with '$$$' speaker
+    index = next(i for i, d in enumerate(ori) if d["Speaker"] == "$$$")
 
-            # # parse the JSON string into Python list of dictionaries
-            try:
-                lst = json.loads(s)
-                print(f"\n{len(ori)}")
+    # get all dictionaries before the one with '$$$' speaker
+    speeches_before = ori[:index]
+
+    # get all dictionaries after the one with '$$$' speaker
+    speeches_after = ori[index:]
+
+    if _type_ == "before":
+        ori = speeches_before
+    elif _type_ == "after":
+        ori = speeches_after
+
+    try:
+        lst = json.loads(s)
+        print(f"\n{len(ori)}")
+        print(len(lst))
+
+        if _type_ == "after":
+            # flag variable to indicate the presence of '$$$'
+            has_dollar_key = False
+
+            # iterate over the dictionaries in the list
+            for speech in lst:
+                # if '$$$' is found in any value of the dictionary
+                if "$$$" in speech.values():
+                    has_dollar_key = True
+                    break
+            if not has_dollar_key:
+                lst.insert(0, {"Speaker": "$$$", "Speech": "$$$"})
                 print(len(lst))
 
-                for r in range(len(ori)):
-                    print(f"\n{ori[r]}")
-                    print(f"{lst[r]}\n")
+        print(len(lst) == len(ori))
+        if len(lst) == len(ori):
+            cleaned_dicts = ori
+            for r in range(len(ori)):
+                # print(f"\n{ori[r]}")
+                # print(f"{lst[r]}\n")
+                if cleaned_dicts[r]['Speaker'] == 'unknown':
+                    cleaned_dicts[r]['Speaker'] = lst[r]['Speaker']
+            cleaned_list = [d for d in cleaned_dicts if d['Speaker'] != '$$$']
+            return cleaned_list
+    except:
+        print(f"\nBroken string, thanks GPT-4")
+        return None
 
-                parts.append(lst)
-            except:
-                print(f"Broken string, thanks GPT-4")
-    break
 
+files = glob.glob(f'{folder}/{savename}-GPT-4-pre*.json')
+
+failed_files = []
+for file in files:
+    number = file.split(".")[0].split("-")[-1]
+    print(number)
+    ori_json = f'{folder}/{savename}-epubfragment-{number}.json'
+    postfile = file.replace("pre", "post")
+    with open(file, 'r', encoding='utf-8') as f, open(ori_json, 'r') as g, open(postfile, 'r', encoding='utf-8') as h:
+
+        ori = json.loads(g.read())
+        predata = json.load(f)
+        postdata = json.load(h)
+
+        for i in range(len(predata)):
+
+            pre_s = clean_up(predata[i])
+            post_s = clean_up(postdata[i])
+
+            # print(pre_s)
+            # print("\n\n")
+            # print(ori)
+
+            # # parse the JSON string into Python list of dictionaries
+            pre_part = check_original(pre_s, ori, "before")
+            post_part = check_original(post_s, ori, "after")
+
+            if pre_part is None:
+                failed_files.append(f"\n{file} \n{pre_part}")
+                if post_part is None:
+                    failed_files.append(f"\n{postfile} \n{post_part}")
+            else:
+                cleaned_json = f'{folder}/{savename}-ready-for-audio-{number}.json'
+                print(cleaned_json)
+                with open(cleaned_json, 'w') as k:
+                    json.dump(pre_part + post_part, k)
+
+failed_json = f'{folder}/{savename}-double-check-errors.json'
+print(failed_json)
+with open(failed_json, 'w') as l:
+    json.dump(failed_files, l)
+
+    # break
 
 
 
